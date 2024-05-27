b80a63f9c3e1d6a93f971376041d384e1b8ba542
Shunki Tada <69703740+Shunkicreate@users.noreply.github.com>
2024-05-27 15:57:29 +0900

    投稿一覧APIの実装 (#4)
    * 投稿一覧APIの実装
    
    * format
    
    ---------
    
    Co-authored-by: shunki-tada <shunki.tada@dena.jp>

---
+++ backend/internal/interfaces/post_repository.go
@@ -0,0 +1,10 @@
+package interfaces
+
+import (
+	"myapp/internal/entities"
+)
+
+type PostRepository interface {
+	// Get(id int) (*entities.Post, error)
+	List() ([]*entities.Post, error)
+}
---
+++ backend/internal/usecases/post_usecase.go
@@ -0,0 +1,21 @@
+package usecases
+
+import (
+	"myapp/internal/entities"
+	"myapp/internal/interfaces"
+)
+
+type PostUsecase struct {
+	repository interfaces.PostRepository
+}
+
+func NewPostUsecase(r interfaces.PostRepository) *PostUsecase {
+	return &PostUsecase{
+		repository: r,
+	}
+}
+
+func (u *PostUsecase) GetList() (
+	[]*entities.Post, error) {
+	return u.repository.List()
+}

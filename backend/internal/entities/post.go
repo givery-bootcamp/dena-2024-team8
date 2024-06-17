a181441cf63dc37f4ba1498ea782b657c15864ee
sota-a-oizumi <sota.a.oizumi@dena.com>
2024-06-17 14:15:59 +0900

    feat: add User

--- backend/internal/entities/post.go
+++ backend/internal/entities/post.go
@@ -7,6 +7,7 @@ import (
 type Post struct {
 	Id        int       `json:"id"`
 	UserId    int       `json:user_id`
+	User      User      `gorm:"foreignKey:UserId"`
 	Title     string    `json:title`
 	Body      string    `json:body`
 	CreatedAt time.Time `json:created_at`

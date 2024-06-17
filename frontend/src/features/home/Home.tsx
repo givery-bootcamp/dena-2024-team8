a34591673ef27f8c481e79f5083a7b349c070fff
waruru <skw.dev.121@gmail.com>
2024-06-17 17:00:13 +0900

    refactor: 不要なコードを削除して可読性を上げる

--- frontend/src/features/home/Home.tsx
+++ frontend/src/features/home/Home.tsx
@@ -1,33 +1,21 @@
-import { useEffect } from 'react';
-
-import { useAppDispatch } from '../../shared/hooks';
-import { APIService } from '../../shared/services';
-import Sidebar from '../../components/Sidebar';
-import PostForm from '../../components/form/PostForm';
-import PostList from '../../features/posts/Posts';
+import Sidebar from "../../components/Sidebar";
+import PostForm from "../../components/form/PostForm";
+import PostList from "../../features/posts/Posts";
 
 export function Home() {
-
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(APIService.getHello());
-  }, [dispatch]);
-
   return (
     <div className="container-xxl">
-    <div className="row">
-      <div className="col-2 border text-center">
-        <Sidebar />
+      <div className="row">
+        <div className="col-2 border text-center">
+          <Sidebar />
+        </div>
+        <div className="col-8 border bg-white vh-100 p-3">
+          <PostForm />
+          <hr />
+          <PostList />
+        </div>
+        <div className="col-2 border"></div>
       </div>
-      <div className="col-8 border bg-white vh-100 p-3">
-        <PostForm />
-        <hr />
-        <PostList />
-      </div>
-      <div className="col-2 border" >
-      </div>
-    </div>
     </div>
   );
 }

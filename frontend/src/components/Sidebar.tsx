b38db78a09004ab873c1246d391ee895699cb862
kenta-yamada <kenta.yamada@dena.com>
2024-06-17 16:44:06 +0900

    ulタグを追加

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,14 +1,16 @@
 export default function Sidebar() {
     return (
         <nav className="nav flex-column">
-          <li>
-            <a className="nav-link btn flat" href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="nav-link" href="/search">Search</a>
-          </li>
+          <ul>
+            <li>
+              <a className="nav-link btn flat" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="/search">Search</a>
+            </li>
+          </ul>
         </nav>
     );
 }

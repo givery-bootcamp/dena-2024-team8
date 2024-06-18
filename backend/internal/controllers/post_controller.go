3efa82e054599fa0107b1f85f28270d3ceeccf93
shunki-tada <shunki.tada@dena.jp>
2024-06-18 11:35:20 +0900

    alartを消す

--- backend/internal/controllers/post_controller.go
+++ backend/internal/controllers/post_controller.go
@@ -13,12 +13,12 @@ import (
 // swaggo用の型定義
 type Post struct {
 	Id        int       `json:"id"`
-	UserId    int       `json:user_id`
-	Title     string    `json:title`
-	Body      string    `json:body`
-	CreatedAt time.Time `json:created_at`
-	UpdatedAt time.Time `json:update_at`
-	DeletedAt time.Time `json:deleted_at`
+	UserId    int       `json:"user_id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
 // PostList godoc

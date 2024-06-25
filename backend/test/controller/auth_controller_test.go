2335959f611f7009d64ad00690b13c00a1905f78
sota-a-oizumi <sota.a.oizumi@dena.com>
2024-06-25 14:59:48 +0900

    fix: test cases

--- backend/test/controller/auth_controller_test.go
+++ backend/test/controller/auth_controller_test.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func init() {
@@ -33,32 +32,62 @@ func TestSignIn(t *testing.T) {
 	req, err := http.NewRequest(http.MethodPost, "/signin", nil)
 	assert.NoError(t, err)
 
-	testCases := []struct{ username, password string }{
-		{"taro", "password"},
-		{"hanako", "PASSWORD"},
+	type TestCase struct {
+		name           string
+		expectResponse int
+		username       string
+		password       string
+	}
+
+	testCases := []TestCase{
+		{
+			name:           "valid user1",
+			expectResponse: http.StatusOK,
+			username:       "taro",
+			password:       "password",
+		},
+		{
+			name:           "valid user2",
+			expectResponse: http.StatusOK,
+			username:       "hanako",
+			password:       "PASSWORD",
+		},
+		{
+			name:           "invalid user1",
+			expectResponse: http.StatusBadRequest,
+			username:       "",
+			password:       "password",
+		},
+		{
+			name:           "invalid user2",
+			expectResponse: http.StatusBadRequest,
+			username:       "taro",
+			password:       "",
+		},
 	}
 
 	for _, tc := range testCases {
-		// テストリクエストにbodyを追加
-		req.PostForm = make(map[string][]string)
-		req.PostForm.Add("username", tc.username)
-		req.PostForm.Add("password", tc.password)
+		t.Run(tc.name, func(t *testing.T) {
+			// テストリクエストにbodyを追加
+			req.PostForm = make(map[string][]string)
+			req.PostForm.Add("username", tc.username)
+			req.PostForm.Add("password", tc.password)
 
-		// レスポンスのレコーダーを作成
-		w := httptest.NewRecorder()
+			// レスポンスのレコーダーを作成
+			w := httptest.NewRecorder()
 
-		// テストリクエストを実行
-		r.ServeHTTP(w, req)
+			// テストリクエストを実行
+			r.ServeHTTP(w, req)
 
-		ru := entities.User{}
-		err = json.Unmarshal(w.Body.Bytes(), &ru)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		assert.Equal(t, http.StatusOK, w.Code)
-		assert.Equal(t, tc.username, ru.Name)
-		err := bcrypt.CompareHashAndPassword([]byte(ru.Password), []byte(tc.password))
-		assert.NoError(t, err)
+			ru := entities.User{}
+			err = json.Unmarshal(w.Body.Bytes(), &ru)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, tc.expectResponse, w.Code)
+			if tc.expectResponse == http.StatusOK {
+				assert.Equal(t, tc.username, ru.Name)
+			}
+		})
 	}
 }

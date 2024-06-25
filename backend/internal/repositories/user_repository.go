e6faaf7ae9a99527500ac917aefd05f7bd87b72d
shunki-tada <shunki.tada@dena.jp>
2024-06-25 13:48:23 +0900

    ハッシュ関数作った

--- backend/internal/repositories/user_repository.go
+++ backend/internal/repositories/user_repository.go
@@ -5,6 +5,7 @@ import (
 	"myapp/internal/entities"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -66,4 +67,29 @@ func (r *UserRepository) Get(id int) (*entities.User, error) {
 	}
 
 	return ConvertUserRepositoryModelToEntity(user), nil
+}
+// この関数は、UserRepository型のメソッドとして定義されています。FindByIPassという名前の関数です。
+// この関数は、与えられたユーザー名とパスワードを使用して、データベース内のユーザーを検索します。まず、与えられたパスワードをハッシュ化し、そのハッシュ値を出力します。次に、usersテーブルから指定されたユーザー名に一致するユーザーを検索します。
+// もし検索結果がエラーでない場合、ユーザーのパスワードと与えられたパスワードを比較します。もしパスワードが一致しない場合、エラーを返します。一致する場合は、ConvertUserRepositoryModelToEntity関数を使用して、データベースモデルをエンティティオブジェクトに変換して返します。
+// この関数は、ユーザーが存在しない場合やエラーが発生した場合には、適切な値（nilやエラーオブジェクト）を返します。
+func (r *UserRepository) FindByIPass(username string, password string) (*entities.User, error) {
+	// SignUpの時にここの処理を使って、パスワードをハッシュ化してDBに保存する
+	// hash, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+	// println(string(hash))
+
+	var user User
+	result := r.Conn.Where("name = ?", username, password).First(user)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		} else {
+			return nil, result.Error
+		}
+	}
+	// ユーザのパスワードと与えられたパスワードを比較
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, err
+	}
+	return ConvertUserRepositoryModelToEntity(&user), nil
 }

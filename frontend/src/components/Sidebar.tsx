64762d7cfe7c464a976957305068a50dd3f1130a
Wataru Sasaki <68587102+waruru@users.noreply.github.com>
2024-06-26 02:04:57 +0000

    feat: 画面レイアウトの微調整

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,12 +1,59 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Sidebar = () => {
+  const navigator = useNavigate();
+  const location = useLocation();
+  const [activePath, setActivePath] = useState<string>("");
+
+  useEffect(() => {
+    setActivePath(location.pathname);
+    console.log(location.pathname);
+  }, [location]);
+
   return (
-    <div className="w-40">
+    <div className="h-full min-w-48 border-r pr-2">
+      <div className="py-4 border-b-4 border-gray-300">
+        <b>メニュー</b>
+      </div>
       <nav>
-        <ul>
-          <li className="mb-2"><Link to ={"/"} className="text-blue-500">Home</Link></li>
-          <li><a href="#" className="text-blue-500">Search</a></li>
+        <ul className="mx-4 py-4 flex flex-col gap-4">
+          <li>
+            <button
+              onClick={() => navigator("/")}
+              className={`rounded py-3 border w-full active:bg-blue-200 active:shadow-inner active:shadow-black ${
+                activePath === "/"
+                  ? "text-blue-700 shadow-black shadow-inner bg-blue-200"
+                  : "text-blue-500 shadow shadow-gray-400 hover:bg-blue-100"
+              }`}
+            >
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => navigator("/search")}
+              className={`rounded py-3 border w-full active:bg-blue-200 active:shadow-inner active:shadow-black ${
+                activePath === "/search"
+                  ? "text-blue-700 shadow-black shadow-inner bg-blue-200"
+                  : "text-blue-500 shadow shadow-gray-400 hover:bg-blue-100"
+              }`}
+            >
+              Search
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => navigator("/signin")}
+              className={`rounded py-3 border w-full active:bg-blue-200 active:shadow-inner active:shadow-black ${
+                activePath === "/signin"
+                  ? "text-blue-700 shadow-black shadow-inner bg-blue-200"
+                  : "text-blue-500 shadow shadow-gray-400 hover:bg-blue-100"
+              }`}
+            >
+              test
+            </button>
+          </li>
         </ul>
       </nav>
     </div>

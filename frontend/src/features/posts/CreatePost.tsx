fc13216e0f7a80ff27c3f7a4473a953133b45d8c
hiroto-kuge <hiroto.kuge@dena.com>
2024-06-28 16:25:20 +0900

    [add]細かい変更を加えました

--- frontend/src/features/posts/CreatePost.tsx
+++ frontend/src/features/posts/CreatePost.tsx
@@ -1,8 +1,10 @@
+import PostForm from "../../components/form/PostForm"
+
 export const CreatePost = () =>{
 
     return (
         <div>
-            新規投稿作成画面だよ
+            <PostForm />
         </div>
     )
 }

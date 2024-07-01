fed4e6719ae44fce7100261a649608610d81d656
nntto <53688020+nntto@users.noreply.github.com>
2024-07-01 10:31:37 +0900

    検索機能を/postsに統合し、クエリに応じてPostエンティティを返す (#92) (#107)
    * feat: integrate /posts and /search
    
    * elasticsearch で日本語検索に対応
    
    * fix: limit and offset
    
    * feat: 投稿検索のエンドポイントを/postsに統合
    
    * 検索機能を/postsに統合し、クエリに応じてPostエンティティを返す (#92)
    
    * feat: integrate /posts and /search
    
    * elasticsearch で日本語検索に対応
    
    * fix: limit and offset
    
    ---------
    
    Co-authored-by: sota-a-oizumi <sota.a.oizumi@dena.com>
    Co-authored-by: nntto <53688020+nntto@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sota-a-oizumi <sota.a.oizumi@dena.com>
    Co-authored-by: Sota Oizumi <60801230+sotazum@users.noreply.github.com>
    Co-authored-by: waruru <skw.dev.121@gmail.com>
    Co-authored-by: Wataru Sasaki <68587102+waruru@users.noreply.github.com>
    Co-authored-by: Shunki Tada <69703740+Shunkicreate@users.noreply.github.com>

--- backend/internal/usecases/post_usecase.go
+++ backend/internal/usecases/post_usecase.go
@@ -1,8 +1,16 @@
 package usecases
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
 	"myapp/internal/entities"
+	"myapp/internal/external"
 	"myapp/internal/interfaces"
+	"strconv"
+	"strings"
 )
 
 type PostUsecase struct {
@@ -34,4 +42,80 @@ func (u *PostUsecase) Update(title, body string, userId, postId int) (*entities.
 
 func (u *PostUsecase) Delete(id int) error {
 	return u.repository.Delete(id)
+}
+
+func (u *PostUsecase) Search(query string) ([]int, error) {
+	var searchQuery string
+	if query == "" {
+		// Query is empty, search for all documents
+		searchQuery = `{
+			"query": {
+				"match_all": {}
+			}
+		}`
+	} else {
+		searchQuery = fmt.Sprintf(`{
+			"query": {
+				"multi_match": {
+					"query": "%s",
+					"fields": ["title", "body"]
+				}
+			}
+		}`, query)
+	}
+
+	var buf strings.Builder
+	buf.WriteString(searchQuery)
+
+	res, err := external.ES.Search(
+		external.ES.Search.WithContext(context.Background()),
+		external.ES.Search.WithIndex("posts"),
+		external.ES.Search.WithBody(strings.NewReader(buf.String())),
+		external.ES.Search.WithTrackTotalHits(true),
+		external.ES.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Printf("Search error: %v", err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			log.Printf("Error parsing the response body: %v", err)
+			return nil, err
+		} else {
+			log.Printf("Error: %s: %s", res.Status(), e["error"].(map[string]interface{})["reason"])
+			return nil, errors.New("error")
+		}
+	}
+
+	var r map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Printf("Error parsing the response body: %v", err)
+		return nil, err
+	}
+
+	postsIds := []int{}
+
+	hits, ok := r["hits"].(map[string]interface{})
+	if !ok || hits["hits"] == nil {
+		// No hits found, handle empty result case
+		return postsIds, nil
+	}
+
+	for _, hit := range hits["hits"].([]interface{}) {
+		source := hit.(map[string]interface{})["_source"].(map[string]interface{})
+		fmt.Printf("source: %v\n", source["id"])
+		id, _ := source["id"].(string) // Type assertion with fallback
+		intId, err := strconv.Atoi(id)
+		if err != nil {
+			log.Printf("Error parsing id: %v", err)
+			continue
+		}
+		postsIds = append(postsIds, intId)
+	}
+
+	return postsIds, nil
 }

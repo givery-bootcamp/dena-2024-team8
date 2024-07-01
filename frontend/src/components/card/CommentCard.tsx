bfdbd82b0054a1dd407f4f69fcc9d0fb7e80d732
waruru <skw.dev.121@gmail.com>
2024-07-01 10:20:19 +0900

    日付表示を整形

--- frontend/src/components/card/CommentCard.tsx
+++ frontend/src/components/card/CommentCard.tsx
@@ -8,7 +8,7 @@ export const CommentCard = ({ comment }: CommentCardProps) => {
   return (
     <div className="border p-4 mb-4 rounded shadow-sm">
       <p className="mb-2">{comment.body}</p>
-      <time className="text-gray-500">{comment.created_at}</time>
+      <time className="text-gray-500">{new Date(comment.created_at).toLocaleString()}</time>
     </div>
   );
 };
